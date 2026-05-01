0853ae4c5694f5001da6f9c9883ab392d09a7a68
alexborodach30 <sagopien04@gmail.com>
2026-05-01 20:42:52 +0300

    Chunk Metadata class init.

--- rag/rag_config.py
+++ rag/rag_config.py
@@ -1,8 +1,18 @@
-from dataclasses import dataclass
-from typing import List, Dict
+from dataclasses import dataclass, field
+from typing import List, Dict, Literal
 
 
 @dataclass
 class RAGResponse:
     answer: str
     sources: List[Dict]
+
+@dataclass
+class ChunkMetadata:
+    source: str
+    file: str
+    chunk_type: Literal["structure", "sentence", "window"]
+    header: str | None
+    level: int | None
+    article_number: str | None
+    topics: List[str] = field(default_factory=list)
