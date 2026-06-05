6f36354a2f9c6531e177aa5ebf4ec94a3f1bf0a7
alexborodach30 <sagopien04@gmail.com>
2026-06-05 10:01:44 +0300

    Logger main file init.

---
+++ backend/app/logger_config.py
@@ -0,0 +1,31 @@
+import logging
+import os
+
+os.makedirs("logs", exist_ok=True)
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    filename = name.replace(".", "_")
+
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+    )
+
+    file_handler = logging.FileHandler(
+        f"logs/{filename}.log",
+        encoding="utf-8"
+    )
+
+    console_handler = logging.StreamHandler()
+
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
