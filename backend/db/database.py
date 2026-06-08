be181bad2892d6f5cc7cff72fae49bd980454356
alexborodach30 <sagopien04@gmail.com>
2026-06-08 20:05:20 +0300

    DB poolers init.

--- backend/db/database.py
+++ backend/db/database.py
@@ -3,7 +3,15 @@ from typing import AsyncIterator
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from backend.app.config import settings
 
-engine = create_async_engine(settings.DATABASE_URL, echo=False, future=True)
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=False,
+    pool_size=10,
+    max_overflow=20,
+    pool_timeout=30,
+    pool_recycle=1800,
+    pool_pre_ping=True,
+)
 
 async_session_maker = async_sessionmaker(
     bind=engine,
